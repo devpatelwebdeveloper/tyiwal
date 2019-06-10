20281329eb915b26633bfd8a6b7303bc41edcf3e
Dev Patel <hello@devashishpatel.com>
2019-06-10 12:37:40 -0400

    Initial Repo

---
+++ js/variable.js
@@ -0,0 +1,506 @@
+const final = [
+  {
+    groupName: "Group 1",
+    websiteName: "Expo"
+  },
+  {
+    groupName: "Group 1",
+    websiteName: "Sharpie"
+  },
+  {
+    groupName: "Group 1",
+    websiteName: "Papermate"
+  },
+  {
+    groupName: "Group 1",
+    websiteName: "Contigo"
+  },
+  {
+    groupName: "Group 1",
+    websiteName: "Elmer's"
+  },
+  {
+    groupName: "Group 1",
+    websiteName: "Sistema"
+  },
+  {
+    groupName: "Group 2",
+    websiteName: "Lipton Pure Leaf"
+  },
+  {
+    groupName: "Group 2",
+    websiteName: "Gatorade"
+  },
+  {
+    groupName: "Group 2",
+    websiteName: "Bubly"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Almond Joy"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Give Education Chocolate Bar"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Ice Breakers"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Kit Kat"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Payday"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Reese's"
+  },
+  {
+    groupName: "Group 3",
+    websiteName: "Hershey's popped snack mix"
+  },
+  {
+    groupName: "Group 4",
+    websiteName: "ME to WE"
+  },
+  {
+    groupName: "Group 5",
+    websiteName: "Halls"
+  },
+  {
+    groupName: "Group 5",
+    websiteName: "Nabisco"
+  },
+  {
+    groupName: "Group 5",
+    websiteName: "Nilla"
+  },
+  {
+    groupName: "Group 5",
+    websiteName: "Oreo"
+  },
+  {
+    groupName: "Group 5",
+    websiteName: "Sourpatch"
+  },
+  {
+    groupName: "Group 5",
+    websiteName: "Swedish Fish"
+  },
+  {
+    groupName: "Group 6",
+    websiteName: "Puremoist"
+  },
+  {
+    groupName: "Group 7",
+    websiteName: "Skinny Pop "
+  },
+  {
+    groupName: "Group 8",
+    websiteName: "Lays"
+  },
+  {
+    groupName: "Group 9",
+    websiteName: "Garnier Fructis"
+  },
+  {
+    groupName: "Group 10",
+    websiteName: "Good 2 Grow"
+  },
+  {
+    groupName: "Group 11",
+    websiteName: "Hint"
+  },
+  {
+    groupName: "Group 12",
+    websiteName: "Dunkin Donut Coffee"
+  },
+  {
+    groupName: "Group 13",
+    websiteName: "Wylers's Light"
+  },
+  {
+    groupName: "Group 14",
+    websiteName: "Band-Aid"
+  },
+  {
+    groupName: "Group 14",
+    websiteName: "Neosporin"
+  },
+  {
+    groupName: "Group 14",
+    websiteName: "Neutrogena"
+  },
+  {
+    groupName: "Group 14",
+    websiteName: "Clean & Clear"
+  },
+  {
+    groupName: "Group 15",
+    websiteName: "Scott"
+  },
+  {
+    groupName: "Group 15",
+    websiteName: "Kleenex"
+  },
+  {
+    groupName: "Group 16",
+    websiteName: "Maxwell House"
+  },
+  {
+    groupName: "Group 16",
+    websiteName: "Capri Sun"
+  },
+  {
+    groupName: "Group 17",
+    websiteName: "Monster"
+  },
+  {
+    groupName: "Group 18",
+    websiteName: "Purell"
+  },
+  {
+    groupName: "Group 19",
+    websiteName: "Crest"
+  },
+  {
+    groupName: "Group 19",
+    websiteName: "Oral B"
+  },
+  {
+    groupName: "Group 19",
+    websiteName: "Puffs"
+  },
+  {
+    groupName: "Group 20",
+    websiteName: "Welch's Fruit Snacks"
+  },
+  {
+    groupName: "Group 21",
+    websiteName: "Life Cereal"
+  },
+  {
+    groupName: "Group 21",
+    websiteName: "Cap'n Crunch"
+  },
+  {
+    groupName: "Group 22",
+    websiteName: "RX Bars "
+  },
+  {
+    groupName: "Group 23",
+    websiteName: "Glade"
+  },
+  {
+    groupName: "Group 23",
+    websiteName: "Ziploc"
+  },
+  {
+    groupName: "Group 24",
+    websiteName: "Thermos"
+  },
+  {
+    groupName: "Group 25",
+    websiteName: "Degree"
+  },
+  {
+    groupName: "Group 25",
+    websiteName: "Suave"
+  },
+  {
+    groupName: "Group 26",
+    websiteName: "Nice!"
+  },
+  {
+    groupName: "Group 26",
+    websiteName: "Well At Walgreens Hand Sanitizer"
+  },
+  {
+    groupName: "Group 26",
+    websiteName: "Wexford"
+  },
+  {
+    groupName: "Group 27",
+    websiteName: "Wonderful"
+  },
+  {
+    groupName: "Group 28",
+    websiteName: "Crayola"
+  },
+  {
+    groupName: "Group 29",
+    websiteName: "Jack Links"
+  },
+  {
+    groupName: "Group 30",
+    websiteName: "Ticonderoga"
+  },
+  {
+    groupName: "Group 30",
+    websiteName: "Dixon"
+  },
+  {
+    groupName: "Group 31",
+    websiteName: "Post It"
+  },
+  {
+    groupName: "Group 31",
+    websiteName: "Scotch"
+  },
+  {
+    groupName: "Group 32",
+    websiteName: "Five Star"
+  },
+  {
+    groupName: "Group 33",
+    websiteName: "BIC"
+  },
+  {
+    groupName: "Group 34",
+    websiteName: "Blue Sky"
+  },
+  {
+    groupName: "Group 35",
+    websiteName: "Fiskars "
+  },
+  {
+    groupName: "Group 36",
+    websiteName: "Pentel"
+  },
+  {
+    groupName: "Group 37",
+    websiteName: "Pilot"
+  },
+  {
+    groupName: "Group 38",
+    websiteName: "Popsocket"
+  },
+  {
+    groupName: "Group 39",
+    websiteName: "Zebra"
+  },
+  {
+    groupName: "Group 40",
+    websiteName: "Storex"
+  },
+  {
+    groupName: "Group 41",
+    websiteName: "Butterfinger"
+  },
+  {
+    groupName: "Group 41",
+    websiteName: "Crunch"
+  },
+  {
+    groupName: "Group 41",
+    websiteName: "Nerds"
+  },
+  {
+    groupName: "Group 41",
+    websiteName: "Sweet Tarts"
+  },
+  {
+    groupName: "Group 41",
+    websiteName: "Baby Ruth"
+  },
+  {
+    groupName: "Group 42",
+    websiteName: "Milky Way"
+  },
+  {
+    groupName: "Group 42",
+    websiteName: "Dove Chocolate"
+  },
+  {
+    groupName: "Group 42",
+    websiteName: "Snickers"
+  },
+  {
+    groupName: "Group 42",
+    websiteName: "Twix"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Apple Jacks"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Corn Flakes"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Froot Loops"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Frosted Flakes"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Frosted Mini Wheats"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Nutri Grain"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Pop Tart Cereal"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Raisin Bran"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Rice Krispies"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Cheez It"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Pop Tarts"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Rice Krispie Treats"
+  },
+  {
+    groupName: "Group 43",
+    websiteName: "Pringles"
+  },
+  {
+    groupName: "Group 44",
+    websiteName: "Palmolive"
+  },
+  {
+    groupName: "Group 45",
+    websiteName: "Sandisk"
+  },
+  {
+    groupName: "Group 45",
+    websiteName: "Infinitive"
+  },
+  {
+    groupName: "Group 46",
+    websiteName: "Airborne"
+  },
+  {
+    groupName: "Group 46",
+    websiteName: "Lysol"
+  },
+  {
+    groupName: "Group 47",
+    websiteName: "Energizer"
+  },
+  {
+    groupName: "Group 47",
+    websiteName: "99 Brands"
+  }
+];
+
+final.forEach(item => {
+  if (item.groupName === "Group 1") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 2") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 3") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 4") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 5") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 6") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 7") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 8") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 9") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 10") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 11") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 12") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 13") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 14") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 15") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 16") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 17") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 18") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 19") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 20") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 21") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 22") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 23") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 24") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 25") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 26") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 27") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 28") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 29") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 30") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 31") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 32") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 33") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 34") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 35") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 36") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 37") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 38") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 39") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 40") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 41") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 42") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 43") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 44") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 45") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 46") {
+    item.value = "https://www.we.org";
+  } else if (item.groupName === "Group 47") {
+    item.value = "https://www.we.org";
+  } else {
+    item.value = "https://www.trackyourimpact.com";
+  }
+});
